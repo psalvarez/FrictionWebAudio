fcbff35f3841e5f127cc824103fb75f1ffba6e2f
Pedro Sanchez <pedsaalv@gmail.com>
2018-08-06 12:54:32 +0200

    Added friction Controls

--- jsap-plugins/frictionPlugin.js
+++ jsap-plugins/frictionPlugin.js
@@ -40,6 +40,7 @@ var frictionPlugin = function (factory, owner) {
     Friction.friction.obj0.fragmentSize = 1;
 
     //This will set the rest of the values automatically, but should be removed when added as controls
+
     setNormalForce(Friction.friction, Friction.force);
     setStribeckVelocity (Friction.friction, Friction.stribeck);
     setStaticCoefficient (Friction.friction, Friction.kStatic);
@@ -56,10 +57,56 @@ var frictionPlugin = function (factory, owner) {
 
 
     //####### PARAMETERS ########//
+
     let extForceParam = this.parameters.createNumberParameter("externalForce", 1, -3, 3);
-    //extForceParam.bindToAudioParam(this.externalForce);
+
+
+    //Friction Parameters
+    let normForceParam = this.parameters.createNumberParameter("normalForce", 0.5, 0, 1);
+    let stribeckParam = this.parameters.createNumberParameter("stribeck", 0.103036, 0.096, 1);
+    let staticParam = this.parameters.createNumberParameter("static", 0.922222, 0.4, 1);
+    let dynamicParam = this.parameters.createNumberParameter("dynamic", 0.3775, 0.01, 0.5);
+    let breakAwayParam = this.parameters.createNumberParameter("breakAway", 0.725, 0.1, 1);
+    let stiffnessParam = this.parameters.createNumberParameter("stiffness", 1606.618164, 500, 5000);
+    let dissipationParam = this.parameters.createNumberParameter("dissipation", 35.925926, 0, 40);
+    let viscosityParam = this.parameters.createNumberParameter("viscosity", 5.55555, 0, 10);
+    let noisinessParam  = this.parameters.createNumberParameter("noisiness", 0.844167, 0.01, 1);
+
+    //Control functions
+    extForceParam.trigger = function () {
+        let a = 2;
+    }.bind(this);
+
+    normForceParam.trigger = function () {
+        setNormalForce(Friction.friction, normForceParam.value);
+    }.bind(normForceParam);
+    stribeckParam.trigger = function () {
+        setStribeckVelocity(Friction.friction, stribeckParam.value);
+    }.bind(this);
+    staticParam.trigger = function () {
+        setStaticCoefficient(Friction.friction, staticParam.value);
+    }.bind(this);
+    dynamicParam.trigger = function () {
+        setDynamicCoefficient(Friction.friction, dynamicParam.value);
+    }.bind(this);
+    breakAwayParam.trigger = function () {
+        setBreakAway(Friction.friction, breakAwayParam.value);
+    }.bind(this);
+    stiffnessParam.trigger = function () {
+        setStiffness(Friction.friction, stiffnessParam.value);
+    }.bind(this);
+    dissipationParam.trigger = function () {
+        setDissipation(Friction.friction, dissipationParam.value);
+    }.bind(this);
+    viscosityParam.trigger = function () {
+        setViscosity(Friction.friction, viscosityParam.value);
+    }.bind(this);
+    noisinessParam.trigger = function () {
+        setNoisiness(Friction.friction, noisinessParam.value);
+    }.bind(this);
 
     //####### AUDIO CODE #######//
+
     this.frictionNode.onaudioprocess = function (e){
         var outs = new Array (2);
         var audioOut = e.outputBuffer.getChannelData(0);
@@ -67,7 +114,7 @@ var frictionPlugin = function (factory, owner) {
             interactorDSP(Friction.friction, extForceParam.value, 0, 0, 0, 0, 0, outs);
             audioOut[i] = 100000*outs[1]; //For now, we are just picking one pickup point from one object
         }
-        console.log(audioOut[0]);
+        //console.log(audioOut[0]);
     }
 
     //Add these at the bottom of your plugin
