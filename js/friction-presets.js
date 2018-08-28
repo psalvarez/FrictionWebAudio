71af813f6890848761ed38eaf5e6cc2fa112bb9b
Pedro Sanchez <pedsaalv@gmail.com>
2018-08-28 18:25:01 +0100

    New presets added

--- js/friction-presets.js
+++ js/friction-presets.js
@@ -32,7 +32,114 @@
   };
   addPreset(presetObject, 'Drifting/Braking car');
 
-  
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 1000,
+      freq2Param: 2000,
+      freq3Param: 3000,
+      decay1Param: 0.01,
+      decay2Param: 0.01,
+      decay3Param: 0.01,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.8,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Wooden Joint');
+
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 700,
+      freq2Param: 1600,
+      freq3Param: 3000,
+      decay1Param: 0.1,
+      decay2Param: 0.13,
+      decay3Param: 0.06,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.8,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Ceramic Plate');
+
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 500,
+      freq2Param: 2000,
+      freq3Param: 3000,
+      decay1Param: 0.09,
+      decay2Param: 0.13,
+      decay3Param: 0.16,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.8,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Metal bowl');
+
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 1000,
+      freq2Param: 2000,
+      freq3Param: 4000,
+      decay1Param: 0.14,
+      decay2Param: 0.15,
+      decay3Param: 0.17,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.8,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Thin glass cup');
+
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 500,
+      freq2Param: 2000,
+      freq3Param: 4000,
+      decay1Param: 0.14,
+      decay2Param: 0.14,
+      decay3Param: 0.15,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.6,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Thick glass cup');
+
+  var presetObject = {};
+  presetObject["RTSFXfriction"] = {
+
+      //These are fixed
+      freq1Param: 500,
+      freq2Param: 600,
+      freq3Param: 910,
+      decay1Param: 0.01,
+      decay2Param: 0.01,
+      decay3Param: 0.01,
+      gain1Param: 0.8,
+      gain2Param: 0.8,
+      gain3Param: 0.8,
+
+      activeModesParam: 3,
+  };
+  addPreset(presetObject, 'Plastic Rubber Surface');
+
 
   if(typeof scene == 'undefined'){
  	addPresetOption(null, presets[0]);
